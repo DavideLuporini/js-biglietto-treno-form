4c2c8870ef496c78c434780a3a699dbb7821a722
illupovero <davideluporini1996@gmail.com>
2022-01-12 15:24:00 +0100

    declaring const

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ console.log('js ok')
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
+
+console.log('js ok');
+
+// ask for the prefered distance
+const distancePrefered = document.querySelector('input').value;
+console.log(distancePrefered);
+
+// ask the age
+const age = document.getElementById('age').value;
+console.log(age);
+
+// declaring price per km
+const pricePerKm = 0.21;
+
+// calculate pre discount price
+const price = (distancePrefered * pricePerKm);
+console.log(price);
