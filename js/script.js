d3d1a691027b247ea710b778a94cf76d0110187b
illupovero <davideluporini1996@gmail.com>
2022-01-12 16:28:17 +0100

    printed in page price and  name

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ console.log('js ok');
 // ask for the name
 const uName = document.getElementById('name').value;
 console.log(uName);
+let DisplayUName = document.getElementById('username')
 
 // ask the DISTANCE
 const distance = document.getElementById('km').value;
@@ -18,9 +19,9 @@ console.log(distance);
 // declaring price per km
 const pricePerKm = 0.21;
 
-// // calculate pre discount price
-// const price = (distance * pricePerKm);
-// console.log(price);
+// calculate pre discount price
+let DisplayPrice = document.getElementById('price')
+console.log(price);
 
 // bottone calcola
 const btnGenerate = document.querySelector('button')
@@ -29,12 +30,16 @@ btnGenerate.addEventListener('click', function() {
     // STAMPARE PRICE IN CASELLA PREZZO
     const uName = document.getElementById('name').value;
     console.log(uName);
+    DisplayUName.append(uName)
 
     const distance = document.getElementById('km').value;
     console.log(distance + ' km');
 
     const price = pricePerKm * distance;
-    console.log(price)
+    DisplayPrice.append(price)
+    console.log(price);
+
+
 })
 
 
