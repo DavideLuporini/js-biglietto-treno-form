4c1ea93ebbdafe836b6c7588dd5a9853bf4c090d
illupovero <davideluporini1996@gmail.com>
2022-01-12 15:31:41 +0100

    connected btns to js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-console.log('js ok')
-
 // Il programma dovrà mostrare una form da compilare con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
@@ -10,8 +8,8 @@ console.log('js ok')
 console.log('js ok');
 
 // ask for the prefered distance
-const distancePrefered = document.querySelector('input').value;
-console.log(distancePrefered);
+const distance = document.querySelector('input').value;
+console.log(distance);
 
 // ask the age
 const age = document.getElementById('age').value;
@@ -21,5 +19,15 @@ console.log(age);
 const pricePerKm = 0.21;
 
 // calculate pre discount price
-const price = (distancePrefered * pricePerKm);
+const price = (distance * pricePerKm);
 console.log(price);
+
+// bottone calcola
+const btnGenerate = document.querySelector('button')
+
+// bottone annulla
+const btnUndo = document.getElementById('undo')
+
+btnGenerate.addEventListener('click', function() {
+
+})
